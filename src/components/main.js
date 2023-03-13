ab11de5e6a0996811dded6fb45842ffb78e6d395
Nahid Hasan <shimantodevjs@gmail.com>
2023-03-13 09:48:23 +0300

    first commit

---
+++ src/components/main.js
@@ -0,0 +1,14 @@
+const Main=()=>(
+    <main>
+        <h1>Fun facts about React</h1>
+        <ul>
+            <li>Was released in 2013</li>
+            <li>Was originally created by Jordan Walke</li>
+            <li>Has well over 100k starts on Github</li>
+            <li>is maintained by Facebook</li>
+            <li>powers thousand of enterprise apps, including mobile apps</li>
+        </ul>
+    </main>
+)
+
+export default Main
---
+++ src/components/nav.js
@@ -0,0 +1,11 @@
+import Logo from '../Logo/React-icon.svg.png'
+
+const Nav=()=>(
+    <nav>
+    <img src={Logo} alt="React" className="logo" />
+    <h3>ReactFacts</h3>
+    <h4>React course- project 1</h4>
+    </nav>
+)
+
+export default Nav
